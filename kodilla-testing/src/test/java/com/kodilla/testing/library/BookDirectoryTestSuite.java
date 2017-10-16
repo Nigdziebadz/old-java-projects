33e6873bbdb7c435ba2022a2d20dae0e8d2ed6cf
“Nigdziebadz <slowaniepowazne@gmail.com>
2017-10-16 20:46:11 +0200

    Test6.6

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
@@ -0,0 +1,44 @@
+package com.kodilla.testing.library;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BookDirectoryTestSuite {
+    private static int testCounter = 0;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        System.out.println("Tests begins.");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        System.out.println("Tests ended");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        System.out.println("starting test #" + testCounter);
+    }
+
+    @Test
+    public void testListBooksWithConditionsReturnList() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testListBooksWithConditionMoreThan20() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testListBooksWithConditionFragmentShorterThan3() {
+        assertTrue(false);
+    }
+
+}
