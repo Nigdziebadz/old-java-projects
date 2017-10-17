e1483d27d99ad415d867ec153214573b35b7ea73
“Nigdziebadz <slowaniepowazne@gmail.com>
2017-10-17 19:39:53 +0200

    End of 6.6

--- kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -12,10 +12,21 @@ public class BookLibrary {
 
     public List<Book> listBooksWithCondition(String titleFragment) {
         List<Book> bookList = new ArrayList<>();
-        Book book = new Book("The book title",
-                "The Book author", 2000);
-        bookList.add(book);
-
+        if (titleFragment.length() < 3) return bookList;
+        List<Book> resultList = libraryDatabase
+                .listBooksWithCondition(titleFragment);
+        if (resultList.size() > 20) return bookList;
+        bookList = resultList;
         return bookList;
+    }
+
+    public List<Book> listBooksInHandsOf(LibraryUser libraryUser) {
+        List<Book> borrowedBookList = new ArrayList<>();
+        List<Book> secondResultList = libraryDatabase
+                .listBooksInHandsOf(libraryUser);
+        borrowedBookList = secondResultList;
+
+        return borrowedBookList;
+
     }
 }
