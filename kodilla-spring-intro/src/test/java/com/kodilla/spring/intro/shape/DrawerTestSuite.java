5340d8a99699a7d57be8b28aa28464a4cdc664dd
nigdziebadz <slowaniepowazne@gmail.com>
2017-11-16 19:03:52 +0100

    End of 10.1

---
+++ kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -0,0 +1,17 @@
+package com.kodilla.spring.intro.shape;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DrawerTestSuite {
+    @Test
+    public void testDoDrawingWithCircle() {
+        //Given
+        Circle circle = new Circle();
+        //When
+        Drawer drawer = new Drawer(circle);
+        String result = drawer.doDrawing();
+        //Then
+        Assert.assertEquals("This is a Circle", result);
+    }
+}
