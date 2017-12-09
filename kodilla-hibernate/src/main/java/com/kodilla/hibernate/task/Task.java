15f141c834028edb40da5820fd5bd5ab203171c0
nigdziebadz <slowaniepowazne@gmail.com>
2017-12-09 18:15:32 +0100

    End of 13.2

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -0,0 +1,64 @@
+package com.kodilla.hibernate.task;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Table(name = "TASKS")
+public class Task {
+    private int id;
+    private String decription;
+    private Date created;
+    private int duration;
+
+    public Task() {
+
+    }
+
+    public Task(String decription, int duration) {
+        this.decription = decription;
+        this.created = new Date();
+        this.duration = duration;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDecription() {
+        return decription;
+    }
+
+    @NotNull
+    @Column(name = "CREATED")
+    public Date getCreated() {
+        return created;
+    }
+
+    @Column(name = "DURATION")
+    public int getDuration() {
+        return duration;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setDecription(String decription) {
+        this.decription = decription;
+    }
+
+    private void setCreated(Date created) {
+        this.created = created;
+    }
+
+    private void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
