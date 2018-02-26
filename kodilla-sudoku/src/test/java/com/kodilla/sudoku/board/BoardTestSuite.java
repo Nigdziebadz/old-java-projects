c5d5ebb85aef770e4a780bc085756e39d1624704
“Nigdziebadz <slowaniepowazne@gmail.com>
2018-02-26 09:10:27 +0100

    Sudoku test data

--- kodilla-sudoku/src/test/java/com/kodilla/sudoku/board/BoardTestSuite.java
+++ kodilla-sudoku/src/test/java/com/kodilla/sudoku/board/BoardTestSuite.java
@@ -6,6 +6,34 @@ import org.junit.Test;
 public class BoardTestSuite {
 
     private void generateTestData(Board board) {
+        board.setElement(0, 3, 2);
+        board.setElement(0, 8, 3);
+        board.setElement(1, 1, 7);
+        board.setElement(1, 4, 9);
+        board.setElement(1, 6, 2);
+        board.setElement(2, 1, 6);
+        board.setElement(2, 2, 1);
+        board.setElement(2, 3, 5);
+        board.setElement(2, 4, 8);
+        board.setElement(2, 7, 4);
+        board.setElement(2, 8, 9);
+        board.setElement(3, 0, 8);
+        board.setElement(3, 5, 4);
+        board.setElement(3, 8, 1);
+        board.setElement(5, 0, 7);
+        board.setElement(5, 3, 8);
+        board.setElement(5, 8, 4);
+        board.setElement(6, 0, 5);
+        board.setElement(6, 1, 9);
+        board.setElement(6, 4, 7);
+        board.setElement(6, 5, 8);
+        board.setElement(6, 6, 3);
+        board.setElement(6, 7, 1);
+        board.setElement(7, 2, 4);
+        board.setElement(7, 4, 3);
+        board.setElement(7, 7, 5);
+        board.setElement(8, 0, 6);
+        board.setElement(8, 5, 1);
 
     }
 
@@ -14,7 +42,7 @@ public class BoardTestSuite {
         SudokuGame sudokuGame = new SudokuGame();
         generateTestData(sudokuGame.getBoard());
         System.out.println(sudokuGame.getBoard());
-        sudokuGame.resolveSudoku();
+//        sudokuGame.resolveSudoku();
 
     }
 }
