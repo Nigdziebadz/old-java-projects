fcc6d9bf50f707cd3c69a18b519c6cef0f08a577
“Nigdziebadz <slowaniepowazne@gmail.com>
2018-03-04 17:58:41 +0100

    End of 23.2

--- kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/invoice/simple/SimpleInvoiceTestSuite.java
@@ -3,7 +3,7 @@ package com.kodilla.stream.invoice.simple;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class SimpleInvoiceTestSuite {
+public class    SimpleInvoiceTestSuite {
 
     @Test
     public void testGetValueToPay() {
